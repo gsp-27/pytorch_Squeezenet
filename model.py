9daf66d655f14925e43c000df86c6b3f776f07e1
Gaurav Pathak <gspat27@gmail.com>
2017-01-23 16:59:23 +0530

    Coded the Squeezenet model

---
+++ model.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.functional as F
+import numpy as np
+import torch.optim as optim
+
+class fire(nn.Module):
+    def __init__(self, inplanes, squeeze_planes, expand_planes):
+        super(fire, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1, stride=1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(squeeze_planes, expand_planes, kernel_size=1, stride=1)
+        self.conv3 = nn.Conv2d(squeeze_planes, expand_planes, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        # using MSR initilization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2./n))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        out1 = self.conv2(x)
+        out2 = self.conv3(x)
+        out = torch.cat([out1, out2], 1)
+        return out
+
+
+class SqueezeNet(nn.Module):
+    def __init__(self):
+        super(SqueezeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=1) # 32
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2) # 16
+        self.fire2 = fire(96, 16, 64)
+        self.fire3 = fire(128, 16, 64)
+        self.fire4 = fire(128, 32, 128)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2) # 8
+        self.fire5 = fire(256, 32, 128)
+        self.fire6 = fire(256, 48, 192)
+        self.fire7 = fire(384, 48, 192)
+        self.fire8 = fire(384, 64, 256)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2) #
+        self.fire9 = fire(512, 64, 256)
+        self.conv2 = nn.Conv2d(512, 10, kernel_size=1, stride=1)
+        self.avg_pool = nn.AvgPool2d(kernel_size=4, stride=4)
+
+    # Using MSR intialization
+    def MSRinit(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+        x = self.fire2(x)
+        x = self.fire3(x)
+        x = self.fire4(x)
+        x = self.maxpool2(x)
+        x = self.fire5(x)
+        x = self.fire6(x)
+        x = self.fire7(x)
+        x = self.fire8(x)
+        x = self.maxpool3(x)
+        x = self.fire9(x)
+        x = self.conv2(x)
+        x = self.avg_pool(x)
+        return x
+
+def squeezenet(pretrained=False):
+    model = SqueezeNet()
+    return model
